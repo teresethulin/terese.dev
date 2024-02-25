73c7ad87afa84e41d9954c04286ebe9398bfba82
teresethulin <terese.thulin@gmail.com>
2024-02-25 16:49:25 +0100

    Restructure folders

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,15 @@ import styles from "./page.module.scss";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-        <Image
-          src="/images/teresethulin.jpg"
-          alt="Picture of Terese Thulin"
-          width={272}
-          height={320}
-          style={{ borderRadius: 8 }}
-        />
-        <h1 className={styles.h1}>Terese Thulin</h1>
-    </main>
+      <main className={styles.main}>
+          <Image
+            src="/images/teresethulin.jpg"
+            alt="Picture of Terese Thulin"
+            width={272}
+            height={320}
+            style={{ borderRadius: 8 }}
+            />
+          <h1 className={styles.h1}>Terese Thulin</h1>
+      </main>
   );
 }
