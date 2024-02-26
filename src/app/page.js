75a938f8091aa6fd3d743a6afa1c0ef4395adbbe
teresethulin <terese.thulin@gmail.com>
2024-02-26 23:51:57 +0100

    Add new components to page layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Hero from "./components/Hero";
+
 import styles from "./page.module.scss";
 
 export default function Home() {
   return (
       <section className={styles.main}>
-          <h1 className={styles.h1}>Digital multi-tool with a wide skillset in programming, design and processes</h1>
+          <Hero/>
       </section>
   );
 }
