dc33fb1fd38f29d78fd1b8fc2b5d2559d6e40337
teresethulin <terese.thulin@gmail.com>
2024-02-26 23:51:41 +0100

    Create Hero component

---
+++ src/app/components/Hero/index.js
@@ -0,0 +1,11 @@
+import styles from "./hero.module.scss";
+
+const Hero = () => {
+  return (
+    <section className={styles.hero}>
+        <h1 className={styles.title}>Digital multi-tool with a wide skillset in programming, design and processes</h1>
+    </section>
+  );
+};
+
+export default Hero;
