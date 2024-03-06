cabf3d978594381162d83738c20e7d8188afebeb
teresethulin <terese.thulin@gmail.com>
2024-03-06 23:12:42 +0100

    Get Hero data from Storyblok

--- src/app/components/Hero/index.js
+++ src/app/components/Hero/index.js
@@ -1,10 +1,22 @@
+import { storyblokEditable } from "@storyblok/react";
+import Wrapper from "../Wrapper";
+
 import styles from "./hero.module.scss";
 
-const Hero = () => {
+const Hero = ({ blok }) => {
+  const alignment = {
+    "--alignHorizontal": blok.alignHorizontal,
+    "--text-align": blok.alignHorizontal.includes("center") ? "center" : "left"
+  }
+
   return (
-    <section className={styles.hero}>
-        <h1 className={styles.title}>Digital multi-tool with a wide skillset in programming, design and processes</h1>
-    </section>
+    <Wrapper {...storyblokEditable(blok)} backgroundColor={blok.backgroundColor ?? ""} textColor={blok.textColor ?? ""}>
+      <div className={styles.hero} style={alignment}>
+        {blok.overline && <span className={styles.overline}>{blok.overline}</span>}
+        {blok.heading && <h1 className={styles.title}>{blok.heading}</h1>}
+        {blok.body && <p className={styles.body}>{blok.body}</p>}
+      </div>
+    </Wrapper>
   );
 };
 
