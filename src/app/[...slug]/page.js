ef5fe2afcb1c3bf6484149c2c2c33318e13ab37a
teresethulin <terese.thulin@gmail.com>
2024-03-23 18:02:35 +0100

    Create logic for dynamic pages

---
+++ src/app/[...slug]/page.js
@@ -0,0 +1,27 @@
+import { getStoryblokApi } from "@storyblok/react/rsc";
+import StoryblokStory from "@storyblok/react/story";
+import { notFound } from 'next/navigation'
+
+import styles from "../page.module.scss";
+
+export default async function Page({ params }) {
+  const slug = params?.slug ? params.slug.join('/') : 'home'
+  const { data } = await fetchData(slug)
+
+  if (!data) {
+    return notFound()
+  }
+
+  return (
+      <section className={styles.main}>
+        <StoryblokStory story={data.story} />
+      </section>
+  );
+}
+
+export async function fetchData(slug) {
+  let sbParams = { version: "draft" };
+
+  const storyblokApi = getStoryblokApi()
+  return storyblokApi.get(`cdn/stories/${slug}`, sbParams)
+}
