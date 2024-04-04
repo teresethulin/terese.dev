5482ceea35387ad5b3e9d8c1b964f1279b8ae762
teresethulin <terese.thulin@gmail.com>
2024-04-04 19:48:08 +0200

    Add image remote patterns to next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,17 @@
 const nextConfig = {
     env: {
         storyblokApiToken: process.env.STORYBLOK_API_TOKEN
-    }
+    },
+    images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'a.storyblok.com',
+        port: '',
+        pathname: '/f/278911/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
