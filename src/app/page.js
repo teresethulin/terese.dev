32c175bdb582e8667a0f6fc5f4353701526019f8
teresethulin <terese.thulin@gmail.com>
2024-04-07 19:47:14 +0200

    Remove unnecessary section from Home

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,11 @@
 import { getStoryblokApi } from "@storyblok/react/rsc";
 import StoryblokStory from "@storyblok/react/story";
 
-import styles from "./page.module.scss";
-
 export default async function Home() {
   const { data } = await fetchData();
 
   return (
-      <section className={styles.container}>
-        <StoryblokStory story={data.story} />
-      </section>
+    <StoryblokStory story={data.story} />
   );
 }
 
