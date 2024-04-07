570dc4b4a1337b2db7b21aac342737526eeb7fea
teresethulin <terese.thulin@gmail.com>
2024-04-07 19:33:12 +0200

    Add error handling for storyblokApi in slug.js

--- src/app/[...slug]/page.js
+++ src/app/[...slug]/page.js
@@ -4,7 +4,7 @@ import { notFound } from 'next/navigation'
 
 export default async function Page({ params }) {
   const slug = params?.slug ? params.slug.join('/') : 'home';
-  const { data } = await fetchData(slug);
+  const data = await fetchData(slug);
 
   if (!data) {
     return notFound();
@@ -19,5 +19,15 @@ export async function fetchData(slug) {
   let sbParams = { version: "draft" };
 
   const storyblokApi = getStoryblokApi();
-  return storyblokApi.get(`cdn/stories/${slug}`, sbParams);
+
+  if (storyblokApi) {
+    try {
+      const { data } = await storyblokApi.get(`cdn/stories/${slug}`, sbParams);
+      return data;   
+    } catch (error) {
+      console.error("Failed to fetch data from Storyblok", error);
+    }
+  } else {
+    throw new Error("Storyblok API is not available");
+  }
 }
