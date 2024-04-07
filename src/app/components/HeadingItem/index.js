800e35f6d91e2dcee4a668f5202486da332cacf7
teresethulin <terese.thulin@gmail.com>
2024-04-07 19:25:46 +0200

    Use classnames to render heading styles instead of attribute selectors

--- src/app/components/HeadingItem/index.js
+++ src/app/components/HeadingItem/index.js
@@ -1,19 +1,27 @@
 import { storyblokEditable } from "@storyblok/react";
+import classNames from "classnames";
 
 import styles from "./heading-item.module.scss";
 
 const HeadingItem = ({ blok }) => {
     const itemStyles = {
         "--column-start": blok?.column_start,
-        "--column-span": blok?.column_span,
-        "--heading-type": blok.type
+        "--column-span": blok?.column_span
     };
 
-    const HeadingElement = blok.type;
+    const headingTypeToClassName = {
+        "h1": styles.h1,
+        "h2": styles.h2,
+        "h3": styles.h3,
+        "h4": styles.h4
+    };
+
+    const HeadingElement = blok?.type;
+    const headingClasses = classNames(styles.heading, headingTypeToClassName[HeadingElement]);
 
     return (
         <>
-            {blok?.text && <HeadingElement {...storyblokEditable(blok)} className={styles.heading} style={itemStyles}>{blok?.text}</HeadingElement>}
+            {blok?.text && <HeadingElement {...storyblokEditable(blok)} className={headingClasses} style={itemStyles}>{blok?.text}</HeadingElement>}
         </>
     );
 };
