c5dacf5c1c62c3176f50b91c951b213a956a8ae3
teresethulin <terese.thulin@gmail.com>
2024-05-26 22:06:15 +0200

    Add heading to Card

--- src/app/components/Card/index.js
+++ src/app/components/Card/index.js
@@ -12,6 +12,48 @@ const Card = ({ blok }) => {
         "--margin-bottom-desktop": blok?.marginBottomDesktop
     };
 
+    const HeadingTag = (level) => {
+        switch(level) {
+            case 1:
+                return 'h1';
+            case 2:
+                return 'h2';
+            case 3:
+                return 'h3';
+            case 4:
+                return 'h4';
+            case 5:
+                return 'h5';
+            case 6:
+                return 'h6';
+            default:
+                return 'span';
+        }
+    };
+
+    const headingTypeToClassName = {
+        "h1": styles.h1,
+        "h2": styles.h2,
+        "h3": styles.h3,
+        "h4": styles.h4
+    };
+
+    const headings = blok?.text?.content?.map((item, index) => {
+        if (item.type === "heading") {
+          const Tag = HeadingTag(item?.attrs?.level);
+          const headingClass = headingTypeToClassName[Tag];
+
+            return (
+                <Tag key={index} {...storyblokEditable(item)} className={headingClass}>
+                    {item.content?.map((textItem, textIndex) => (
+                        <React.Fragment key={textIndex}>{textItem?.text}</React.Fragment>
+                    ))}
+                </Tag>
+            );
+        }
+        return null;
+    });
+
     const paragraphs = blok?.text?.content?.map((item, index) => {
         if (item.type === "paragraph") {
             return (
@@ -31,6 +73,7 @@ const Card = ({ blok }) => {
         <article className={styles.card}>
           {blok?.date && <span className={styles.date}>{blok?.date}</span>}
           {blok?.overline && <span className={styles.overline}>{blok?.overline}</span>}
+          {headings}
           {paragraphs}
         </article>
       </div>
