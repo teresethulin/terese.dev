7c6a2c4233b02ba7c50ad886c88a11c3daf1885f
teresethulin <terese.thulin@gmail.com>
2024-05-26 15:21:30 +0200

    Enable anchorID for anchor links

--- src/app/components/Storyblok/Grid.js
+++ src/app/components/Storyblok/Grid.js
@@ -1,15 +1,15 @@
 import { storyblokEditable, StoryblokComponent } from "@storyblok/react/rsc";
 
 import styles from "./grid.module.scss";
- 
+
 const Grid = ({ blok }) => {
   return (
-    <section {...storyblokEditable(blok)} className={styles.grid} style={{ backgroundColor: blok?.backgroundColor }}>
+    <section {...storyblokEditable(blok)} id={blok?.anchorId} className={styles.grid} style={{ backgroundColor: blok?.backgroundColor }}>
       {blok?.columns?.map((nestedBlok) => (
         <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
       ))}
     </section>
   );
 };
- 
+
 export default Grid;
