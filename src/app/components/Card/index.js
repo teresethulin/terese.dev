7f23144553f305590e6944fdbf749eb95f850cb1
teresethulin <terese.thulin@gmail.com>
2024-07-02 13:35:44 +0200

    Cleanup Card semantics and styling

--- src/app/components/Card/index.js
+++ src/app/components/Card/index.js
@@ -98,16 +98,12 @@ const Card = ({ blok }) => {
     ));
 
     return (
-      <div {...storyblokEditable(blok)} className={styles.wrapper} style={itemStyles}>
-        {blok?.pretitle && <aside className={styles.pretitle}>{blok?.pretitle}</aside>}
-        <article className={styles.card}>
-          {blok?.date && <span className={styles.date}>{blok?.date}</span>}
-          {blok?.overline && <span className={styles.overline}>{blok?.overline}</span>}
-          {headings}
-          {paragraphs}
-          {bulletLists}
+        <article {...storyblokEditable(blok)} className={styles.card} style={itemStyles}>
+            {blok?.overline && <span className={styles.overline}>{blok?.overline}</span>}
+            {headings}
+            {paragraphs}
+            {bulletLists}
         </article>
-      </div>
     );
 };
 
