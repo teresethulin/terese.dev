9da39b3c480a2c8fdba7d1a95db7a58316ceabba
teresethulin <terese.thulin@gmail.com>
2024-08-04 18:09:22 +0200

    Use camelCase for svg props

--- public/icons/chevron.js
+++ public/icons/chevron.js
@@ -1,5 +1,5 @@
 export const Chevron = ({ className, color }) => (
   <svg className={className} width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M1 1L7 7L13 1" stroke={color} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M1 1L7 7L13 1" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
   </svg>
 );
