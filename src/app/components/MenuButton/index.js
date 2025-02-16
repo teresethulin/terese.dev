e421f734693710ec604ec07e8d1254e7a8a66b50
teresethulin <terese.thulin@gmail.com>
2025-02-16 17:23:13 +0100

    Update design MenuButton

--- src/app/components/MenuButton/index.js
+++ src/app/components/MenuButton/index.js
@@ -15,6 +15,7 @@ const MenuButton = React.forwardRef(({ onClick, isOpen, lineColor, buttonColor }
 
   return (
     <button ref={ref} aria-label="menu" className={buttonClasses} onClick={onClick} style={buttonStyle}>
+      <p>{isOpen ? "Close" : "Menu"}</p>
         <span className={styles.burger}>
             <span className={styles.line}/>
             <span className={styles.line}/>
